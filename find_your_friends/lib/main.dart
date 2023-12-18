92b9f9c4f42ec706b232c320d4837aaa5b9d41dd
WhoStoleMyBrain <marcowetter@gmx.de>
2023-12-18 13:25:36 +0100

    Configured flutter project with firebase authentication

--- find_your_friends/lib/main.dart
+++ find_your_friends/lib/main.dart
@@ -1,7 +1,19 @@
-import 'package:find_your_friends/widgets/google_maps_widget.dart';
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() {
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'widgets/google_maps_firebase_widget.dart';
+import 'package:location/location.dart' as loc;
+import 'package:permission_handler/permission_handler.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -32,28 +44,132 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final loc.Location location = loc.Location();
+  StreamSubscription<loc.LocationData>? _locationSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _requestPermission();
+    // location.changeSettings(interval: 300, accuracy: loc.LocationAccuracy.high);
+    // location.enableBackgroundMode(enable: true);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('live location tracker'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.8,
-              height: MediaQuery.of(context).size.width * 0.8,
-              child: const GoogleMapsWidget(),
-            )
-          ],
-        ),
+      body: Column(
+        children: [
+          TextButton(
+              onPressed: () {
+                _getLocation();
+              },
+              child: const Text('add my location')),
+          TextButton(
+              onPressed: () {
+                _listenLocation();
+              },
+              child: const Text('enable live location')),
+          TextButton(
+              onPressed: () {
+                _stopListening();
+              },
+              child: const Text('stop live location')),
+          Expanded(
+              child: StreamBuilder(
+            stream:
+                FirebaseFirestore.instance.collection('location').snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              return ListView.builder(
+                  itemCount: snapshot.data?.docs.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title:
+                          Text(snapshot.data!.docs[index]['name'].toString()),
+                      subtitle: Row(
+                        children: [
+                          Text(snapshot.data!.docs[index]['latitude']
+                              .toString()),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Text(snapshot.data!.docs[index]['longitude']
+                              .toString()),
+                        ],
+                      ),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.directions),
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => GoogleMapsFirebaseWidget(
+                                  snapshot.data!.docs[index].id)));
+                        },
+                      ),
+                    );
+                  });
+            },
+          )),
+        ],
       ),
     );
+  }
+
+  _getLocation() async {
+    try {
+      final loc.LocationData locationResult = await location.getLocation();
+      await FirebaseFirestore.instance.collection('location').doc('user1').set({
+        'latitude': locationResult.latitude,
+        'longitude': locationResult.longitude,
+        'name': 'john'
+      }, SetOptions(merge: true));
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  Future<void> _listenLocation() async {
+    _locationSubscription = location.onLocationChanged.handleError((onError) {
+      if (kDebugMode) {
+        print(onError);
+      }
+      _locationSubscription?.cancel();
+      setState(() {
+        _locationSubscription = null;
+      });
+    }).listen((loc.LocationData currentlocation) async {
+      await FirebaseFirestore.instance.collection('location').doc('user1').set({
+        'latitude': currentlocation.latitude,
+        'longitude': currentlocation.longitude,
+        'name': 'john'
+      }, SetOptions(merge: true));
+    });
+  }
+
+  _stopListening() {
+    _locationSubscription?.cancel();
+    setState(() {
+      _locationSubscription = null;
+    });
+  }
+
+  _requestPermission() async {
+    var status = await Permission.location.request();
+    if (status.isGranted) {
+      if (kDebugMode) {
+        print('done');
+      }
+    } else if (status.isDenied) {
+      _requestPermission();
+    } else if (status.isPermanentlyDenied) {
+      openAppSettings();
+    }
   }
 }
