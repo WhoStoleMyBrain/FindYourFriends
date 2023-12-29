089e262169a1d8b3bad8e2d54c702341e811f878
WhoStoleMyBrain <marcowetter@gmx.de>
2023-12-29 10:02:57 +0100

    Changed: Renamed form bloc

--- find_your_friends/lib/features/form_validation/bloc/form_validation_event.dart
+++
@@ -1,4 +0,0 @@
-part of 'form_validation_bloc.dart';
-
-@immutable
-sealed class FormValidationEvent {}
--- find_your_friends/lib/features/form_validation/bloc/form_validation_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'form_validation_bloc.dart';
-
-@immutable
-sealed class FormValidationState {}
-
-final class FormValidationInitial extends FormValidationState {}
