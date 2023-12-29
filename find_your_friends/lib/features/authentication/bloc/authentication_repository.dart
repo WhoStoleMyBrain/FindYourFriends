5534097f8fecbea77ca90c987d8fc0f0fe18e398
WhoStoleMyBrain <marcowetter@gmx.de>
2023-12-29 10:00:58 +0100

    Added basic authentication repository

---
+++ find_your_friends/lib/features/authentication/bloc/authentication_repository.dart
@@ -0,0 +1,51 @@
+import 'package:find_your_friends/models/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'authentication_service.dart';
+
+class AuthenticationRepositoryImpl implements AuthenticationRepository {
+  //TODO: Add Dependency injection for both authentication and database service
+  AuthenticationService authService = AuthenticationService();
+  DatabaseService dbService = DatabaseService();
+
+  @override
+  Stream<UserModel> getCurrentUser() {
+    return authService.retrieveCurrentUser();
+  }
+
+  @override
+  Future<UserCredential?> signUp(UserModel user) {
+    try {
+      return authService.signUp(user);
+    } on FirebaseAuthException catch (e) {
+      throw FirebaseAuthException(code: e.code, message: e.message);
+    }
+  }
+
+  @override
+  Future<UserCredential?> signIn(UserModel user) {
+    try {
+      return authService.signIn(user);
+    } on FirebaseAuthException catch (e) {
+      throw FirebaseAuthException(code: e.code, message: e.message);
+    }
+  }
+
+  @override
+  Future<void> signOut() {
+    return authService.signOut();
+  }
+
+  @override
+  Future<String?> retrieveUserName(UserModel user) {
+    return dbService.retrieveUserName(user);
+  }
+}
+
+abstract class AuthenticationRepository {
+  Stream<UserModel> getCurrentUser();
+  Future<UserCredential?> signUp(UserModel user);
+  Future<UserCredential?> signIn(UserModel user);
+  Future<void> signOut();
+  Future<String?> retrieveUserName(UserModel user);
+}
