5d0e87153ebc7940e8412aab0f52eaad104f5580
WhoStoleMyBrain <marcowetter@gmx.de>
2023-12-29 09:47:02 +0100

    Added basic authentication bloc logic

--- find_your_friends/lib/features/authentication/bloc/authentication_event.dart
+++ find_your_friends/lib/features/authentication/bloc/authentication_event.dart
@@ -1,4 +1,21 @@
 part of 'authentication_bloc.dart';
 
-@immutable
-sealed class AuthenticationEvent {}
+// @immutable
+// sealed class AuthenticationEvent {}
+
+abstract class AuthenticationEvent extends Equatable {
+  const AuthenticationEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AuthenticationStarted extends AuthenticationEvent {
+  @override
+  List<Object> get props => [];
+}
+
+class AuthenticationSignedOut extends AuthenticationEvent {
+  @override
+  List<Object> get props => [];
+}
