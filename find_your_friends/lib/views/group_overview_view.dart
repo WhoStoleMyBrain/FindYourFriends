4f0e0871a8cfb71e0cb6649bf43f07f88a8deb03
WhoStoleMyBrain <marcowetter@gmx.de>
2024-01-02 21:18:50 +0100

    Added: Basic Group ListView builder for values returned from firebase stream

--- find_your_friends/lib/views/group_overview_view.dart
+++ find_your_friends/lib/views/group_overview_view.dart
@@ -1,36 +1,86 @@
 import 'package:find_your_friends/features/database/bloc/database_bloc.dart';
+import 'package:find_your_friends/features/database/database_repository.dart';
+import 'package:find_your_friends/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class GroupOvervieweView extends StatelessWidget {
-  const GroupOvervieweView({super.key});
+import '../features/authentication/bloc/authentication_bloc.dart';
+import '../models/group_model.dart';
+import 'welcome_view.dart';
+
+class GroupOverviewView extends StatelessWidget {
+  GroupOverviewView({super.key});
+
+  final DatabaseRepository _databaseRepository = DatabaseRepositoryImpl();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Create your own Group'),
-      ),
-      body: BlocBuilder<DatabaseBloc, DatabaseState>(
-        builder: (context, state) {
-          if (state is DatabaseGroupFetchedSuccess) {
+    return BlocConsumer<AuthenticationBloc, AuthenticationState>(
+        listener: (context, state) {
+      if (state is AuthenticationFailure) {
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => const WelcomeView()),
+            (Route<dynamic> route) => false);
+      }
+    }, buildWhen: ((previous, current) {
+      if (current is AuthenticationFailure) {
+        return false;
+      }
+      return true;
+    }), builder: (context, state) {
+      return Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          actions: <Widget>[
+            IconButton(
+                icon: const Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                ),
+                onPressed: () async {
+                  context
+                      .read<AuthenticationBloc>()
+                      .add(AuthenticationSignedOut());
+                })
+          ],
+          systemOverlayStyle:
+              const SystemUiOverlayStyle(statusBarColor: Colors.blue),
+          title: const Text("Group Overview"),
+        ),
+        body: StreamBuilder(
+          stream: _databaseRepository.retrieveGroupData(),
+          builder: (context, AsyncSnapshot<List<GroupModel>> snapshot) {
+            if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
             return ListView.builder(
-              itemCount: 3,
+              itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
                 return Card(
                   child: ListTile(
-                    title: Text(state.groups[index].groupName!),
-                    subtitle: Text(state.groups[index].description!),
-                    trailing: Text(state.groups[index].creator!),
+                    title: Text(snapshot.data![index].groupName!),
+                    subtitle: Text(snapshot.data![index].description!),
+                    trailing: Text(snapshot.data![index].creator!),
                   ),
                 );
               },
             );
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-    );
+          },
+        ),
+        floatingActionButton: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(
+                  builder: (context) => const HomeView(),
+                ),
+                (route) => false);
+          },
+          child: const Text('Home'),
+        ),
+      );
+    });
   }
 }
