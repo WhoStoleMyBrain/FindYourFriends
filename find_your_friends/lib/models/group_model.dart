dd2a3dc5a95ff4f9ac9bafa416db36f83e347141
WhoStoleMyBrain <marcowetter@gmx.de>
2024-01-02 21:17:28 +0100

    Added: Creator ID to group model

--- find_your_friends/lib/models/group_model.dart
+++ find_your_friends/lib/models/group_model.dart
@@ -5,33 +5,43 @@ class GroupModel {
   String? creator;
   List<String>? members;
   String? description;
+  String? creatorId;
 
-  GroupModel({this.groupName, this.creator, this.members, this.description});
+  GroupModel(
+      {this.groupName,
+      this.creator,
+      this.members,
+      this.description,
+      this.creatorId});
 
   Map<String, dynamic> toMap() {
     return {
       'groupName': groupName,
       'creator': creator,
       'description': description,
+      'creatorId': creatorId,
     };
   }
 
   GroupModel.fromDocumentSnapshot(DocumentSnapshot<Map<String, dynamic>> doc)
       : groupName = doc.data()!["groupName"],
         creator = doc.data()!["creator"],
-        description = doc.data()!["description"];
+        description = doc.data()!["description"],
+        creatorId = doc.data()!["description"];
 
   GroupModel copyWith({
     String? groupName,
     String? creator,
     List<String>? members,
     String? description,
+    String? creatorId,
   }) {
     return GroupModel(
       groupName: groupName ?? this.groupName,
       creator: creator ?? this.creator,
       members: members ?? this.members,
       description: description ?? this.description,
+      creatorId: creatorId ?? this.creatorId,
     );
   }
 }
