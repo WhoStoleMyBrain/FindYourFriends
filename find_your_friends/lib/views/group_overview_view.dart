b9c7cf1fe41649fa06bd21a71a9574474abe756e
WhoStoleMyBrain <marcowetter@gmx.de>
2024-01-04 20:30:15 +0100

    Added: GroupLocationBloc Listener and navigation on ListTile tap to GroupDetailView of tapped group

--- find_your_friends/lib/views/group_overview_view.dart
+++ find_your_friends/lib/views/group_overview_view.dart
@@ -1,11 +1,12 @@
-import 'package:find_your_friends/features/database/bloc/database_bloc.dart';
 import 'package:find_your_friends/features/database/database_repository.dart';
+import 'package:find_your_friends/views/group_detail_view.dart';
 import 'package:find_your_friends/views/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../features/authentication/bloc/authentication_bloc.dart';
+import '../features/group_location/bloc/group_location_bloc.dart';
 import '../models/group_model.dart';
 import 'welcome_view.dart';
 
@@ -16,71 +17,87 @@ class GroupOverviewView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<AuthenticationBloc, AuthenticationState>(
-        listener: (context, state) {
-      if (state is AuthenticationFailure) {
-        Navigator.of(context).pushAndRemoveUntil(
-            MaterialPageRoute(builder: (context) => const WelcomeView()),
-            (Route<dynamic> route) => false);
-      }
-    }, buildWhen: ((previous, current) {
-      if (current is AuthenticationFailure) {
-        return false;
-      }
-      return true;
-    }), builder: (context, state) {
-      return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          actions: <Widget>[
-            IconButton(
-                icon: const Icon(
-                  Icons.logout,
-                  color: Colors.white,
-                ),
-                onPressed: () async {
-                  context
-                      .read<AuthenticationBloc>()
-                      .add(AuthenticationSignedOut());
-                })
-          ],
-          systemOverlayStyle:
-              const SystemUiOverlayStyle(statusBarColor: Colors.blue),
-          title: const Text("Group Overview"),
-        ),
-        body: StreamBuilder(
-          stream: _databaseRepository.retrieveGroupData(),
-          builder: (context, AsyncSnapshot<List<GroupModel>> snapshot) {
-            if (!snapshot.hasData || snapshot.data!.isEmpty) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  child: ListTile(
-                    title: Text(snapshot.data![index].groupName!),
-                    subtitle: Text(snapshot.data![index].description!),
-                    trailing: Text(snapshot.data![index].creator!),
+    return MultiBlocListener(
+        listeners: [
+          BlocListener<AuthenticationBloc, AuthenticationState>(
+            listener: (context, state) {
+              if (state is AuthenticationFailure) {
+                Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(
+                        builder: (context) => const WelcomeView()),
+                    (Route<dynamic> route) => false);
+              }
+            },
+          ),
+          BlocListener<GroupLocationBloc, GroupLocationState>(
+            listener: (context, state) {
+              if (state is GroupLocationUpdateState) {
+                if (state.groupId != '') {
+                  Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(
+                          builder: (context) => GroupDetailView()),
+                      (Route<dynamic> route) => false);
+                }
+              }
+            },
+          )
+        ],
+        child: Scaffold(
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            actions: <Widget>[
+              IconButton(
+                  icon: const Icon(
+                    Icons.logout,
+                    color: Colors.white,
                   ),
+                  onPressed: () async {
+                    context
+                        .read<AuthenticationBloc>()
+                        .add(AuthenticationSignedOut());
+                  })
+            ],
+            systemOverlayStyle:
+                const SystemUiOverlayStyle(statusBarColor: Colors.blue),
+            title: const Text("Group Overview"),
+          ),
+          body: StreamBuilder(
+            stream: _databaseRepository.retrieveGroupData(),
+            builder: (context, AsyncSnapshot<List<GroupModel>> snapshot) {
+              if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return const Center(
+                  child: CircularProgressIndicator(),
                 );
-              },
-            );
-          },
-        ),
-        floatingActionButton: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).pushAndRemoveUntil(
-                MaterialPageRoute(
-                  builder: (context) => const HomeView(),
-                ),
-                (route) => false);
-          },
-          child: const Text('Home'),
-        ),
-      );
-    });
+              }
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text(snapshot.data![index].groupName!),
+                      subtitle: Text(snapshot.data![index].description!),
+                      trailing: Text(snapshot.data![index].creator!),
+                      onTap: () {
+                        context.read<GroupLocationBloc>().add(
+                            GroupLocationInitialEvent(
+                                groupId: snapshot.data![index].groupId!));
+                      },
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+          floatingActionButton: ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(
+                    builder: (context) => const HomeView(),
+                  ),
+                  (route) => false);
+            },
+            child: const Text('Home'),
+          ),
+        ));
   }
 }
